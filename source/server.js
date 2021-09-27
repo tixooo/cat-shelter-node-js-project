7757773038240d6b6cce2844628556de3985c8e6
Konstantin Stefanov <konstantin.stefanov87@gmail.com>
2021-09-27 16:57:01 +0300

    static page and html and css up and runing

--- source/server.js
+++ source/server.js
@@ -6,11 +6,15 @@ const port = 5000;
 app.use(express.static(__dirname + '/Public'))
 
 app.get('/', (req, res) => {
-
-    let catShelter = path.resolve(__dirname + '/Views/catShelter.html');
-    let addBreed = path.resolve(__dirname + '/Views/addCat.html')
+    let catShelter = path.resolve(__dirname + '/Views/home/index.html');
     res.sendFile(catShelter);
-    // res.sendFile(addBreed);
 });
-
+app.get('/addCat', (req, res) => {
+    let addCat = path.resolve(__dirname + '/Views/addCat.html')
+    res.sendFile(addCat);
+});
+app.get('/addBreed', (req, res) => {
+    let addBreed = path.resolve(__dirname + '/Views/addBreed.html')
+    res.sendFile(addBreed);
+});
 app.listen(port, console.log(`Server runing on port ${port}...`));
