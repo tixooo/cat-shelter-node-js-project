c9913c87d4e4496516cd92f18783e226c92719d7
Konstantin Stefanov <konstantin.stefanov87@gmail.com>
2021-09-27 14:13:13 +0300

    server connected to static pages and css

--- source/server.js
+++ source/server.js
@@ -3,14 +3,14 @@ const path = require('path')
 const app = express();
 const port = 5000;
 
-app.use(express.static(__dirname + '/content'))
+app.use(express.static(__dirname + '/Public'))
 
 app.get('/', (req, res) => {
 
-    let addCat = path.resolve(__dirname, './Views/catShelter.html');
-    let addBreed = path.resolve(__dirname, './Views/addCat.html')
-    res.sendFile(addCat);
-    res.sendFile(addBreed);
+    let catShelter = path.resolve(__dirname + '/Views/catShelter.html');
+    let addBreed = path.resolve(__dirname + '/Views/addCat.html')
+    res.sendFile(catShelter);
+    // res.sendFile(addBreed);
 });
 
 app.listen(port, console.log(`Server runing on port ${port}...`));
